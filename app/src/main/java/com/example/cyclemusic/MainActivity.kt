92c652f5b6ee61f4b85c3a8e6213de349ad60589
ishidafuu <ishida@gmail.com>
2023-05-07 21:53:20 +0900

    テンポ調整シークバー追加
    テンポ調整

--- app/src/main/java/com/example/cyclemusic/MainActivity.kt
+++ app/src/main/java/com/example/cyclemusic/MainActivity.kt
@@ -3,6 +3,7 @@ package com.example.cyclemusic
 import android.annotation.SuppressLint
 import android.os.Bundle
 import android.util.Log
+import android.widget.SeekBar
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.WindowCompat
 import androidx.core.view.WindowInsetsCompat
@@ -10,35 +11,51 @@ import androidx.core.view.WindowInsetsControllerCompat
 import androidx.media3.common.MediaItem
 import androidx.media3.common.MimeTypes
 import androidx.media3.common.Player
+import androidx.media3.common.PlaybackParameters
 import androidx.media3.common.util.Util
 import androidx.media3.exoplayer.ExoPlayer
 import androidx.media3.exoplayer.trackselection.DefaultTrackSelector
 import com.example.cyclemusic.databinding.ActivityMainBinding
+
 private const val TAG = "MainActivity"
-//class MainActivity : AppCompatActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContentView(R.layout.activity_main)
-//    }
-//}
+
+// MainActivityクラスはAppCompatActivityを継承します
 class MainActivity : AppCompatActivity() {
 
+    // viewBindingを遅延初期化
     private val viewBinding by lazy(LazyThreadSafetyMode.NONE) {
         ActivityMainBinding.inflate(layoutInflater)
     }
 
+    // プレーヤーのリスナーを初期化
     private val playbackStateListener: Player.Listener = playbackStateListener()
     private var player: ExoPlayer? = null
 
+    // プレーヤーの状態を保持する変数
     private var playWhenReady = true
     private var currentItem = 0
     private var playbackPosition = 0L
 
+    // アクティビティが作成されたときに呼び出されるコールバック
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(viewBinding.root)
+
+        // SeekBarのリスナーを設定
+        viewBinding.seekBar.setOnSeekBarChangeListener(object : SeekBar.OnSeekBarChangeListener {
+            override fun onProgressChanged(seekBar: SeekBar?, progress: Int, fromUser: Boolean) {
+                if (fromUser) {
+                    val speed = progress / 100f
+                    setPlaybackSpeedWithoutChangingPitch(speed)
+                }
+            }
+
+            override fun onStartTrackingTouch(seekBar: SeekBar?) {}
+            override fun onStopTrackingTouch(seekBar: SeekBar?) {}
+        })
     }
 
+    // アクティビティが開始されたときに呼び出されるコールバック
     public override fun onStart() {
         super.onStart()
         if (Util.SDK_INT > 23) {
@@ -46,6 +63,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // アクティビティが再開されたときに呼び出されるコールバック
     public override fun onResume() {
         super.onResume()
         hideSystemUi()
@@ -54,6 +72,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // アクティビティが一時停止されたときに呼び出されるコールバック
     public override fun onPause() {
         super.onPause()
         if (Util.SDK_INT <= 23) {
@@ -61,6 +80,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // アクティビティが停止されたときに呼び出されるコールバック
     public override fun onStop() {
         super.onStop()
         if (Util.SDK_INT > 23) {
@@ -68,6 +88,7 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    // プレーヤーを初期化する関数
     private fun initializePlayer() {
         val trackSelector = DefaultTrackSelector(this).apply {
             setParameters(buildUponParameters().setMaxVideoSizeSd())
@@ -90,6 +111,7 @@ class MainActivity : AppCompatActivity() {
             }
     }
 
+    // プレーヤーを解放する関数
     private fun releasePlayer() {
         player?.let { exoPlayer ->
             playbackPosition = exoPlayer.currentPosition
@@ -101,6 +123,16 @@ class MainActivity : AppCompatActivity() {
         player = null
     }
 
+    // プレーヤーの再生速度を設定するメソッド (ピッチを変更しない)
+    fun setPlaybackSpeedWithoutChangingPitch(speed: Float) {
+        player?.let { exoPlayer ->
+            val currentPitch = exoPlayer.playbackParameters.pitch
+            val playbackParameters = PlaybackParameters(speed, currentPitch)
+            exoPlayer.setPlaybackParameters(playbackParameters)
+        }
+    }
+    
+    // システムUIを非表示にする関数
     @SuppressLint("InlinedApi")
     private fun hideSystemUi() {
         WindowCompat.setDecorFitsSystemWindows(window, false)
@@ -112,14 +144,15 @@ class MainActivity : AppCompatActivity() {
     }
 }
 
+// プレーヤーの再生状態が変更されたときにログに出力するリスナー関数
 private fun playbackStateListener() = object : Player.Listener {
     override fun onPlaybackStateChanged(playbackState: Int) {
         val stateString: String = when (playbackState) {
-            ExoPlayer.STATE_IDLE -> "ExoPlayer.STATE_IDLE      -"
+            ExoPlayer.STATE_IDLE -> "ExoPlayer.STATE_IDLE -"
             ExoPlayer.STATE_BUFFERING -> "ExoPlayer.STATE_BUFFERING -"
-            ExoPlayer.STATE_READY -> "ExoPlayer.STATE_READY     -"
-            ExoPlayer.STATE_ENDED -> "ExoPlayer.STATE_ENDED     -"
-            else -> "UNKNOWN_STATE             -"
+            ExoPlayer.STATE_READY -> "ExoPlayer.STATE_READY -"
+            ExoPlayer.STATE_ENDED -> "ExoPlayer.STATE_ENDED -"
+            else -> "UNKNOWN_STATE -"
         }
         Log.d(TAG, "changed state to $stateString")
     }
