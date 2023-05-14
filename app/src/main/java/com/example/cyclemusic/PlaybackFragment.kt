d5745566a63b8177f74f84733e077ca779541062
ishidafuu <ishida@gmail.com>
2023-05-14 11:03:27 +0900

    exoPlayer

--- app/src/main/java/com/example/cyclemusic/PlaybackFragment.kt
+++ app/src/main/java/com/example/cyclemusic/PlaybackFragment.kt
@@ -1,13 +1,13 @@
-import android.media.MediaPlayer
+import android.content.Context
+import android.content.SharedPreferences
+import android.graphics.Color
+import android.net.Uri
 import android.os.Bundle
 import android.os.Environment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Button
-import android.content.Context
-import android.content.SharedPreferences
-import android.graphics.Color
 import android.widget.LinearLayout
 import android.widget.TextView
 import androidx.fragment.app.Fragment
@@ -15,22 +15,34 @@ import androidx.fragment.app.activityViewModels
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 import com.example.cyclemusic.R
+import androidx.media3.common.MediaItem
+import androidx.media3.common.MimeTypes
+import androidx.media3.common.PlaybackParameters
+import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.common.util.Util
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.trackselection.DefaultTrackSelector
 import java.io.File
 import java.util.Random
 
-
 data class Song(val name: String, val path: String)
+
 class PlaybackFragment : Fragment() {
 
-    private lateinit var mediaPlayer: MediaPlayer
+    private lateinit var exoPlayer: ExoPlayer
     private lateinit var songRecyclerView: RecyclerView
     private lateinit var stopButton: Button
     private lateinit var playButton: Button
+    private lateinit var tempoUpButton: Button
+    private lateinit var tempoDownButton: Button
+    private lateinit var defaultTempoButton: Button
     private lateinit var sharedPreferences: SharedPreferences
     private lateinit var songList: MutableList<Song>
     private val viewModel: SharedViewModel by activityViewModels()
     private var folderPath: String? = null
-
+    private var playbackSpeed: Float = 1f
+    
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
@@ -44,11 +56,13 @@ class PlaybackFragment : Fragment() {
         songRecyclerView = view.findViewById(R.id.songRecyclerView)
         stopButton = view.findViewById(R.id.stopButton)
         playButton = view.findViewById(R.id.playButton)
-
-        mediaPlayer = MediaPlayer().apply {
-            setOnCompletionListener {
-                playRandomMedia()
-            }
+        tempoUpButton = view.findViewById(R.id.tempoUpButton)
+        tempoDownButton = view.findViewById(R.id.tempoDownButton)
+        defaultTempoButton = view.findViewById(R.id.defaultTempoButton)
+        
+        exoPlayer = ExoPlayer.Builder(requireContext()).build().apply {
+            setMediaItem(MediaItem.fromUri(""))
+            prepare()
         }
         songList = ArrayList()
 
@@ -65,6 +79,18 @@ class PlaybackFragment : Fragment() {
 
         playButton.setOnClickListener {
             playRandomMedia()
+        }
+
+        tempoUpButton.setOnClickListener {
+            increaseTempo()
+        }
+
+        tempoDownButton.setOnClickListener {
+            decreaseTempo()
+        }
+
+        defaultTempoButton.setOnClickListener {
+            resetTempo()
         }
 
         sharedPreferences = requireContext().getSharedPreferences("CycleMusicPrefs", Context.MODE_PRIVATE)
@@ -99,7 +125,6 @@ class PlaybackFragment : Fragment() {
         return songFiles
     }
 
-
     private fun setFolderPath(folderPath: String) {
         this.folderPath = folderPath
         sharedPreferences.edit().putString("LastFolderPath", folderPath).apply()
@@ -111,7 +136,7 @@ class PlaybackFragment : Fragment() {
         songAdapter.songs.addAll(songList as ArrayList<Song>)
         songAdapter.notifyDataSetChanged()
     }
-    
+
     fun updateFileList() {
         viewModel.selectedFolderPath.value?.let {
             setFolderPath(it)
@@ -119,9 +144,7 @@ class PlaybackFragment : Fragment() {
     }
 
     fun stopMedia() {
-
-        mediaPlayer.stop()
-        mediaPlayer.reset() 
+        exoPlayer.stop()
         val songAdapter = (songRecyclerView.adapter as SongAdapter)
         val previousSelectedPosition = songAdapter.selectedPosition
         songAdapter.selectedPosition = -1
@@ -129,19 +152,17 @@ class PlaybackFragment : Fragment() {
     }
 
     fun playMedia(path: String) {
-        mediaPlayer.stop()
-        mediaPlayer.reset() 
-        mediaPlayer.setDataSource(path)
-        mediaPlayer.prepare()
-        mediaPlayer.start()
+        val mediaItem = MediaItem.fromUri(Uri.parse(path))
+        exoPlayer.setMediaItem(mediaItem)
+        exoPlayer.prepare()
+        exoPlayer.playWhenReady = true
     }
-
 
     fun playRandomMedia() {
         if (songList.isEmpty()) {
             return
         }
-        
+
         val songAdapter = songRecyclerView.adapter as SongAdapter
         if (songAdapter.itemCount == 0) {
             return
@@ -154,10 +175,26 @@ class PlaybackFragment : Fragment() {
         songAdapter.selectedPosition = randomIndex
         songAdapter.notifyItemChanged(previousSelectedPosition)
         songAdapter.notifyItemChanged(randomIndex)
-        
+
         songRecyclerView.layoutManager?.scrollToPosition(randomIndex)
     }
 
+    private fun increaseTempo() {
+        playbackSpeed += 0.02f
+        exoPlayer.setPlaybackParameters(PlaybackParameters(playbackSpeed))
+    }
+
+    private fun decreaseTempo() {
+        playbackSpeed -= 0.02f
+        if (playbackSpeed < 0.1f) playbackSpeed = 0.1f  // prevent playback speed from going too low
+        exoPlayer.setPlaybackParameters(PlaybackParameters(playbackSpeed))
+    }
+
+    private fun resetTempo() {
+        playbackSpeed = 1f
+        exoPlayer.setPlaybackParameters(PlaybackParameters(playbackSpeed))
+    }
+    
     private fun onSongSelected(position: Int) {
         try {
             val selectedFile = songList[position]
@@ -169,22 +206,22 @@ class PlaybackFragment : Fragment() {
 
     override fun onPause() {
         super.onPause()
-        if (mediaPlayer.isPlaying) {
-            mediaPlayer.pause()
+        if (exoPlayer.isPlaying) {
+            exoPlayer.playWhenReady = false
         }
     }
 
     override fun onResume() {
         super.onResume()
         updateFileList()
+        if (!exoPlayer.isPlaying) {
+            exoPlayer.playWhenReady = true
+        }
     }
 
     override fun onDestroy() {
         super.onDestroy()
-        if (mediaPlayer.isPlaying) {
-            mediaPlayer.stop()
-        }
-        mediaPlayer.release()
+        exoPlayer.release()
     }
 
     private inner class SongViewHolder(view: View) : RecyclerView.ViewHolder(view) {
@@ -216,3 +253,4 @@ class PlaybackFragment : Fragment() {
         override fun getItemCount() = songs.size
     }
 }
+
